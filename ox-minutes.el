ed7fc20b20eaafe4c99bc8e5fd0a4570e511b8b2
Kaushal Modi <kaushal.modi@gmail.com>
2017-03-22 16:17:26 -0400

    Add emacs 24 dependency for lexical binding

--- ox-minutes.el
+++ ox-minutes.el
@@ -6,6 +6,7 @@
 ;; URL: https://github.com/kaushalmodi/ox-minutes
 ;; Version: 0.1
 ;; Keywords: org, exporter, notes
+;; Package-Requires: ((emacs "24"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
